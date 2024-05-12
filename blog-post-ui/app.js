e525346b83ccbb943b1ca5b57d5b92ad40a3cbe4
CuriousMagpie <eilatan@gmail.com>
2024-05-11 23:10:41 -0400

    add vue & vite & tailwindcss

--- blog-post-ui/app.js
+++ blog-post-ui/app.js
@@ -1,7 +1,25 @@
 const app = new Vue({
   el: '#app',
   data: {
+    date: '',
     title: '',
+    description: '',
+    tags: [],
+    layout: ['posts.njk', 'page.njk'],
+    content: '',
   },
-  methods: {},
+  methods: {
+    newPost() {
+      this.newPost.push({
+        date: this.date,
+        title: this.title,
+        description: this.description,
+        tags: this.tags,
+        layout: this.layout,
+        content: this.content,
+      });
+      this.newPost = '';
+      console.log(date);
+    },
+  },
 });
---
+++ blog-post-ui/src/components/TextEditor.vue
@@ -0,0 +1,17 @@
+<template>
+  <vue-simplemde v-model="content" ref="markdownEditor" />
+</template>
+
+<script>
+import VueSimplemde from 'vue-simplemde';
+
+export default {
+  components: {
+    VueSimplemde,
+  },
+};
+</script>
+
+<style>
+@import '~simplemde/dist/simplemde.min.css';
+</style>
